62e9a1d9ed77bbf7c0352cbd5a3db62a897c751a
Kinga Borkowska <kinga.borkowska@best.com.pl>
2021-02-08 19:24:25 +0100

    initial commit

---
+++ src/main/java/org/example/UnionFind.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class UnionFind
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
